c4fd3e88d8a4d889df7e75cb8858542281e603a8
plotier <plotier15@gmail.com>
2021-07-18 18:48:26 +0000

    dropdown fixed

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
+import $ from "jquery";
+import Popper from "popper.js";
 
 export const Navbar = () => {
 	return (
@@ -8,9 +13,28 @@ export const Navbar = () => {
 				<span className="navbar-brand mb-0 h1">Star Wars</span>
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Favorites</button>
-				</Link>
+				<div className="dropdown">
+					<button
+						className="btn btn-secondary dropdown-toggle"
+						type="button"
+						id="dropdownMenuButton"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Dropdown button
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+						<a className="dropdown-item" href="#">
+							Action
+						</a>
+						<a className="dropdown-item" href="#">
+							Another action
+						</a>
+						<a className="dropdown-item" href="#">
+							Something else here
+						</a>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
